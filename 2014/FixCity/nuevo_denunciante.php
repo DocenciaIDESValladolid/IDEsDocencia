ef4f35932ab99e4da3e51e04b6a6ef352e3b436f
juacas <juacas@a4868a5b-8e08-4945-af1d-c9bbebf9cebb>
2014-06-10 08:39:40 +0000

    Grabado en UTF8 para codificación de carácteres internacionales

--- 2014/FixCity/nuevo_denunciante.php
+++ 2014/FixCity/nuevo_denunciante.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Añadir denunciante extra a una denuncia ya notificada</title>
+	<title>AÃ±adir denunciante extra a una denuncia ya notificada</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=1">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	
@@ -22,13 +22,13 @@
 <body>
 <?php
 
-	require('db.php');			// Fichero de conexión a la base de datos
+	require('db.php');			// Fichero de conexiÃ³n a la base de datos
 
-	@ $id_denuncia = $_GET['id_denuncia'];	// Gestión de usuarios
-	@ $id_facebook = $_POST['id_facebook'];	// Gestión de usuarios
+	@ $id_denuncia = $_GET['id_denuncia'];	// GestiÃ³n de usuarios
+	@ $id_facebook = $_POST['id_facebook'];	// GestiÃ³n de usuarios
 	
  
- 	// Inserción de la denunciante en la tabla de denunciantes
+ 	// InserciÃ³n de la denunciante en la tabla de denunciantes
 	$query = "SELECT id_denuncia FROM denunciantes WHERE id_denunciante = '$id_facebook' AND id_denuncia = $id_denuncia";
 	$result = pg_exec($db, $query);
 	$array = pg_fetch_array($result);
@@ -36,11 +36,11 @@
 		echo '<div data-role="page" data-theme="b">
 		<div data-role="header"><h2>DENUNCIA YA APOYADA PREVIAMENTE</h2></div>
 		<div data-role="content">
-		<h2>Gracias por su colaboración, pero ya apoyó esta denuncia.</h2>
+		<h2>Gracias por su colaboraciÃ³n, pero ya apoyÃ³ esta denuncia.</h2>
 		</div>';
 	}
 	else{
-			// Inserción de la denunciante en la tabla de denunciantes
+			// InserciÃ³n de la denunciante en la tabla de denunciantes
 		$query = "INSERT INTO denunciantes (id_denuncia, id_denunciante, fecha) VALUES 
 				($id_denuncia, '$id_facebook','".date("Y-m-d")."')";
 		
@@ -48,7 +48,7 @@
 		echo '<div data-role="page" data-theme="b">
 		<div data-role="header"><h2>DENUNCIA APOYADA</h2></div>
 		<div data-role="content">
-		<h2>Gracias por su colaboración.</h2>
+		<h2>Gracias por su colaboraciÃ³n.</h2>
 		</div>';
 
 	}
