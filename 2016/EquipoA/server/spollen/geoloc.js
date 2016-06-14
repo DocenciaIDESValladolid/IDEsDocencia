29137450c869fe7e1c966f3cfd7149595f1095ff
joselemark <joselu.mark@gmail.com>
2016-06-14 11:06:50 +0200

    Proceso WPS de intersección
    Proceso WPS para hacer la intersección en un radio sobre los usos del
    suelo en España.

--- 2016/EquipoA/server/spollen/geoloc.js
+++ 2016/EquipoA/server/spollen/geoloc.js
@@ -17,9 +17,9 @@ function init(){
 	var pnoa_layer = new OpenLayers.Layer.WMS( "Ortofotos España","http://www.ign.es/wms-inspire/pnoa-ma?", {layers: 'PNOA',transparent: true} );
   	var avisos_layer = new OpenLayers.Layer.WMS( "Avisos","http://localhost:8080/geoserver/wms?", {layers: 'Proyecto:avisos',transparent: true}, {isBaseLayer: false, opacity: 1} );
 	geoloc = new OpenLayers.Layer.Vector('Geolocalicacion');
+	coverwfs = new OpenLayers.Layer.Vector("WFS");
 	
-	
-    map.addLayers([pnoa_layer,layer, avisos_layer,geoloc]);
+    map.addLayers([pnoa_layer,layer, avisos_layer,geoloc,coverwfs]);
 	
     map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
     map.addControl( new OpenLayers.Control.LayerSwitcher() );
@@ -67,7 +67,7 @@ function init(){
         firstGeolocation = false;
         this.bind = true;
     }
-	addCoverFromPoint(e.point,0.001);
+	getIntersectionFeatures(e.point,0.001);
 });
 	
 	geolocate.events.register("locationfailed",this,function() {
@@ -79,7 +79,7 @@ function init(){
     firstGeolocation = true;
     geolocate.watch = false;
     geolocate.activate();
-    }
+}
         
     var pulsate = function(feature) {
     var point = feature.geometry.getCentroid(),
@@ -117,6 +117,90 @@ function init(){
 function getGML3FormatInspire(){
 	// añade soporte del elemento member definido en el esquema Inspire
 	OpenLayers.Format.GML.v3.prototype.readers.wfs.member=OpenLayers.Format.GML.Base.prototype.readers["gml"]["featureMember"];
+	OpenLayers.Format.GML.v3.prototype.readers.feature= {
+            "*": function(node, obj) {
+                // The node can either be named like the featureType, or it
+                // can be a child of the feature:featureType.  Children can be
+                // geometry or attributes.
+                var name;
+                var local = node.localName || node.nodeName.split(":").pop();
+                // Since an attribute can have the same name as the feature type
+                // we only want to read the node as a feature if the parent
+                // node can have feature nodes as children.  In this case, the
+                // obj.features property is set.
+                if (obj.features) {
+                    if (!this.singleFeatureType &&
+                        (OpenLayers.Util.indexOf(this.featureType, local) !== -1)) {
+                        name = "_typeName";
+                    } else if(local === this.featureType) {
+                        name = "_typeName";
+                    }
+                } else {
+                    // Assume attribute elements have one child node and that the child
+                    // is a text node.  Otherwise assume it is a geometry node.
+                    if(node.localName=='geometry'){
+                    	 name = "_geometry";
+                    }
+                       else {
+                            name = "_attribute";
+                        }
+              
+                }
+                if(name) {
+                    this.readers.feature[name].apply(this, [node, obj]);
+                }
+            },
+            "_typeName": function(node, obj) {
+                var container = {components: [], attributes: {}};
+                this.readChildNodes(node, container);
+                // look for common gml namespaced elements
+                if(container.name) {
+                    container.attributes.name = container.name;
+                }
+                var feature = new OpenLayers.Feature.Vector(
+                    container.components[0], container.attributes
+                );
+                if (!this.singleFeatureType) {
+                    feature.type = node.nodeName.split(":").pop();
+                    feature.namespace = node.namespaceURI;
+                }
+                var fid = node.getAttribute("fid") ||
+                    this.getAttributeNS(node, this.namespaces["gml"], "id");
+                if(fid) {
+                    feature.fid = fid;
+                }
+                if(this.internalProjection && this.externalProjection &&
+                   feature.geometry) {
+                    feature.geometry.transform(
+                        this.externalProjection, this.internalProjection
+                    );
+                }
+                if(container.bounds) {
+                    feature.bounds = container.bounds;
+                }
+                obj.features.push(feature);
+            },
+            "_geometry": function(node, obj) {
+                if (!this.geometryName) {
+                    this.geometryName = node.nodeName.split(":").pop();
+                }
+                this.readChildNodes(node, obj);
+            },
+            "_attribute": function(node, obj) {
+                var local = node.localName || node.nodeName.split(":").pop();
+                if (node.childNodes.length==1 && node.childNodes[0].nodeType==3){
+                	var value = this.getChildValue(node);
+                	obj.attributes[local] = value;
+                }else if (local=='landCoverObservation'){
+                	var value= node.childNodes[1].childNodes[1].attributes['xlink:href'].nodeValue;
+                	obj.attributes[local] = value;
+                }
+                else{
+                	 this.readChildNodes(node, obj);
+                }
+                
+            }
+        };
 	// Creo una instancia de GML.v3 con los parámetros no estándar
 	var gmlFormat=new OpenLayers.Format.GML.v3({
 					surface:true,
@@ -128,65 +212,83 @@ function getGML3FormatInspire(){
 	return gmlFormat;
 }
 
-
-function addCoverFromPoint(punto,distancia){
-	var gmlLandcoverFormat= getGML3FormatInspire();
-
-	var punto2 = new OpenLayers.Geometry.Point(-4.3,40);
-		coverwfs = new OpenLayers.Layer.Vector("WFS", {
-		strategies: [new OpenLayers.Strategy.Fixed()],
-		protocol: new OpenLayers.Protocol.WFS({
-			version: "1.1.0",
-			srsName: "EPSG:4326",
-			url:  "http://localhost/wfs",
-			featureType: "LandCoverUnit",
-			featureNS: "http://inspire.ec.europa.eu/schemas/lcv/4.0",
-			outputFormat: "application/gml+xml; version=3.2",
-			readFormat: gmlLandcoverFormat
-		}),
-		filter: new OpenLayers.Filter.Spatial({
-			property: "geometry",
-			type: OpenLayers.Filter.Spatial.DWITHIN,
-			value: punto,
-			distance: distancia,
-			distanceUnits: 'meters'
-		})		
-	});
-	map.addLayer(coverwfs);
-	}
-	
-	function getIntersectionFeatures(lat, lon, distance){
+function _wfsLoaded (resp) {
+	var jstsGeom2 = getJSTSGeom(new OpenLayers.Feature.Vector(bufferGeom));
+	var ponderaciones = new Array();
+    for (var i=0;i<resp.features.length;i++){
+    	var feat=resp.features[i];
+    	var jstsGeom1 = getJSTSGeom(feat);
+    	var inters = jstsGeom1.intersection(jstsGeom2);
+    	var ol=getOLFeature(inters);
+    	if (typeof ol == 'undefined'){
+    			
+    		}else{
+    			feat.geometry = ol.geometry;
+    			coverwfs.addFeatures(feat);
+    		}		
+			
+		var area = feat.geometry.getArea();		
+		var sumaArea =+ area;		
+		ponderaciones[i] = area*0.8;
 		
-		 buffer = client.getProcess('opengeo', 'JTS:buffer');
+    }	
+	
+	operacionArea(sumaArea,ponderaciones);
+
+}	
+
+function operacionArea(sumaArea,ponderaciones){
+	
+	var ponderacion
+	
+	for(var i=0;i<ponderaciones.length;i++){
+		ponderacion =+ ponderaciones[i];
+	}	
+	
+	riesgo = ponderacion/sumaArea;
+	riesgo = riesgo;
+	
+}
+
+function getIntersectionFeatures(punto, distance){
+		client = new OpenLayers.WPSClient({
+        servers: {
+            opengeo: 'http://localhost/geoserver/wps'
+        }
+    });
+	buffer = client.getProcess('opengeo', 'JTS:buffer');
     buffer.execute({
         inputs: {
-            geom: new OpenLayers.Geometry.Point(lat,lon),
-            distance: 0.01
+            geom: punto,
+            distance: distance
         },
         success: function(outputs) {
             // outputs.result is a feature or an array of features for spatial
             // processes.
-			
+			bufferGeom=outputs.result[0].geometry;
 			// Add WFS layer
-					coverwfs = new OpenLayers.Layer.Vector("WFS", {
-				strategies: [new OpenLayers.Strategy.Fixed()],
-				protocol: new OpenLayers.Protocol.WFS({
+			var gmlLandcoverFormat= getGML3FormatInspire();
+
+			
+			protocol = new OpenLayers.Protocol.WFS({
 					version: "1.1.0",
 					srsName: "EPSG:4326",
 					url:  "http://localhost/wfs",
 					featureType: "LandCoverUnit",
 					featureNS: "http://inspire.ec.europa.eu/schemas/lcv/4.0",
 					outputFormat: "application/gml+xml; version=3.2",
-					readFormat: gmlLandcoverFormat
-				}),
-				filter: new OpenLayers.Filter.Spatial({
-					property: "geometry",
-					type: OpenLayers.Filter.Spatial.INTERSECTS,
-					value: outputs.result[0].geometry,
-				})		
-			});
-			map.addLayer(coverwfs);
-			
+					readFormat: gmlLandcoverFormat,
+					defaultFilter:new OpenLayers.Filter.Spatial({
+									property: "geometry",
+									type: OpenLayers.Filter.Spatial.INTERSECTS,
+									value: bufferGeom,
+					})		
+				});
+			var response = protocol.read({
+									maxFeatures: 100,
+									callback: _wfsLoaded
+								});	 
+
         }
     });
 		
@@ -215,3 +317,20 @@ function addCoverFromPoint(punto,distancia){
 </wps:Execute>`
 
 	}
+
+function getOLFeature(JSTSGeom){
+     var wkt= new OpenLayers.Format.WKT();
+     var jstsparser=new jsts.io.WKTWriter();
+     var olgeom=wkt.read(jstsparser.write(JSTSGeom));
+     return olgeom;  
+}
+/*
+Convert OL Feature into JTSGeom
+*/
+function getJSTSGeom(OLfeature){
+        var wkt= new OpenLayers.Format.WKT();
+        var jtsparser=new jsts.io.WKTReader();
+        var wktgeom=wkt.write(OLfeature);
+        var jstsGeom = jtsparser.read(wktgeom);
+        return jstsGeom;
+}
