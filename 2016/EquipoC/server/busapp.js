c7f181ad246a35841e881d5c1e12dc2d99f8545e
Jonathan FRANCOU <Jonathan FRANCOU>
2016-06-15 14:58:22 +0200

    codigo final

--- 2016/EquipoC/server/busapp.js
+++ 2016/EquipoC/server/busapp.js
@@ -58,7 +58,7 @@ var geomGml=request.substr(start,end);
 var ns='xmlns:gml="http://www.opengis.net/gml" xmlns:sch="http://www.ascc.net/xml/schematron" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xs="http://www.w3.org/2001/XMLSchema" srsDimension="2"';
 geomGml.replace('<gml:LineString','<gml:LineString '+ns);
 
-var peticion=`
+var peticionWPS=`
 <?xml version="1.0" encoding="UTF-8"?><wps:Execute version="1.0.0" service="WPS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.opengis.net/wps/1.0.0" xmlns:wfs="http://www.opengis.net/wfs" xmlns:wps="http://www.opengis.net/wps/1.0.0" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc" xmlns:wcs="http://www.opengis.net/wcs/1.1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd">
   <ows:Identifier>gs:IntersectionFeatureCollection</ows:Identifier>
   <wps:DataInputs>
@@ -113,7 +113,7 @@ var peticion=`
               <wps:Input>
                 <ows:Identifier>crs</ows:Identifier>
                 <wps:Data>
-                  <wps:LiteralData>EPSG:3857</wps:LiteralData>
+                  <wps:LiteralData>EPSG:4326</wps:LiteralData>
                 </wps:Data>
               </wps:Input>
 			   <wps:Input>
@@ -158,21 +158,168 @@ var peticion=`
   </wps:ResponseForm>
 </wps:Execute>`;
 
-// hacer POST
+var request = new OpenLayers.Request.POST({
+        url: 'http://localhost/geoserver/wps',
+        data: peticionWPS,
+        headers: {
+            "Content-Type": "text/xml;charset=utf-8"
+        },
+        async: true,
+        callback: function (response) {
+            //read the response from GeoServer
+            var analysisLayer = readFeaturesFromWPSResponse(response.responseText);
+			// feqtures debe tener una coleccion de features con las edificaciones intersectadas
+		// bucle for para sumar poblacion18
+			//procesar.....
+			map.addLayer(analysisLayer);
 
-// parseqr respuesta
-/*var gmlParser=new OpenLayers.Format.GML();
-var resultados=gmlParser.read(geomGml);*/
-// bucle for para sumar poblacion18
+			map.zoomToMaxExtent();
+
+        },
+        failure: function (response) {
+            alert("Something went wrong in the request");
+        }
+    });
 
 
-// anadir al mapa
-//map.addLayer(resultados);
+
+
 }
+function processOLSResponse(response){
+	
+var start=response.indexOf("<gml:LineString>");
+var end=response.indexOf("</RouteGeometry>");
+var geomGml=response.substr(start,end-start);
+//add namespaces
+var ns='srsName="EPSG:4326" xmlns:gml="http://www.opengis.net/gml" xmlns:sch="http://www.ascc.net/xml/schematron" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xs="http://www.w3.org/2001/XMLSchema" srsDimension="2"';
+geomGml=geomGml.replace('<gml:LineString','<gml:LineString '+ns);
 
+var peticionWPS=`<?xml version="1.0" encoding="UTF-8"?><wps:Execute version="1.0.0" service="WPS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.opengis.net/wps/1.0.0" xmlns:wfs="http://www.opengis.net/wfs" xmlns:wps="http://www.opengis.net/wps/1.0.0" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc" xmlns:wcs="http://www.opengis.net/wcs/1.1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd">
+  <ows:Identifier>gs:IntersectionFeatureCollection</ows:Identifier>
+  <wps:DataInputs>
+    <wps:Input>
+      <ows:Identifier>first feature collection</ows:Identifier>
+      <wps:Reference mimeType="text/xml" xlink:href="http://geoserver/wfs" method="POST">
+        <wps:Body>
+          <wfs:GetFeature service="WFS" version="1.0.0" outputFormat="GML2" xmlns:busapp="http://localhost:8080/geoserver/busapp">
+            <wfs:Query typeName="busapp:pate"/>
+          </wfs:GetFeature>
+        </wps:Body>
+      </wps:Reference>
+    </wps:Input>
+    <wps:Input>
+      <ows:Identifier>second feature collection</ows:Identifier>
+      <wps:Reference mimeType="text/xml" xlink:href="http://geoserver/wps" method="POST">
+        <wps:Body>
+          <wps:Execute version="1.0.0" service="WPS">
+            <ows:Identifier>gs:Feature</ows:Identifier>
+            <wps:DataInputs>
+              <wps:Input>
+                <ows:Identifier>geometry</ows:Identifier>
+                <wps:Reference mimeType="text/xml; subtype=gml/3.1.1" xlink:href="http://geoserver/wps" method="POST">
+                  <wps:Body>
+                    <wps:Execute version="1.0.0" service="WPS">
+                      <ows:Identifier>geo:buffer</ows:Identifier>
+                      <wps:DataInputs>
+                        <wps:Input>
+                          <ows:Identifier>geom</ows:Identifier>
+                          <wps:Data>
+                            <wps:ComplexData mimeType="text/xml; subtype=gml/3.1.1">
+							${geomGml}
+                            </wps:ComplexData>
+                          </wps:Data>
+                        </wps:Input>
+                        <wps:Input>
+                          <ows:Identifier>distance</ows:Identifier>
+                          <wps:Data>
+                            <wps:LiteralData>500</wps:LiteralData>
+                          </wps:Data>
+                        </wps:Input>
+                      </wps:DataInputs>
+                      <wps:ResponseForm>
+                        <wps:RawDataOutput mimeType="text/xml; subtype=gml/3.1.1">
+                          <ows:Identifier>result</ows:Identifier>
+                        </wps:RawDataOutput>
+                      </wps:ResponseForm>
+                    </wps:Execute>
+                  </wps:Body>
+                </wps:Reference>
+              </wps:Input>
+              <wps:Input>
+                <ows:Identifier>crs</ows:Identifier>
+                <wps:Data>
+                  <wps:LiteralData>EPSG:4326</wps:LiteralData>
+                </wps:Data>
+              </wps:Input>
+			   <wps:Input>
+      <ows:Identifier>typeName</ows:Identifier>
+      <wps:Data>
+        <wps:LiteralData>buffer</wps:LiteralData>
+      </wps:Data>
+    </wps:Input>
+            </wps:DataInputs>
+            <wps:ResponseForm>
+              <wps:RawDataOutput mimeType="text/xml; subtype=wfs-collection/1.0">
+                <ows:Identifier>result</ows:Identifier>
+              </wps:RawDataOutput>
+            </wps:ResponseForm>
+          </wps:Execute>
+        </wps:Body>
+      </wps:Reference>
+    </wps:Input>
+    <wps:Input>
+      <ows:Identifier>first attributes to retain</ows:Identifier>
+      <wps:Data>
+        <wps:LiteralData>poblacion18</wps:LiteralData>
+      </wps:Data>
+    </wps:Input>
+    <wps:Input>
+      <ows:Identifier>intersectionMode</ows:Identifier>
+      <wps:Data>
+        <wps:LiteralData>FIRST</wps:LiteralData>
+      </wps:Data>
+    </wps:Input>
+    <wps:Input>
+      <ows:Identifier>percentagesEnabled</ows:Identifier>
+      <wps:Data>
+        <wps:LiteralData>true</wps:LiteralData>
+      </wps:Data>
+    </wps:Input>
+  </wps:DataInputs>
+  <wps:ResponseForm>
+    <wps:RawDataOutput mimeType="text/xml; subtype=wfs-collection/1.0">
+      <ows:Identifier>result</ows:Identifier>
+    </wps:RawDataOutput>
+  </wps:ResponseForm>
+</wps:Execute>`;
+
+var request = new OpenLayers.Request.POST({
+        url: 'http://localhost/geoserver/wps',
+        data: peticionWPS,
+        headers: {
+            "Content-Type": "text/xml;charset=utf-8"
+        },
+        async: true,
+        callback: function (response) {
+            //read the response from GeoServer
+            var analysisLayer = readFeaturesFromWPSResponse(response.responseText);
+			// feqtures debe tener una coleccion de features con las edificaciones intersectadas
+		// bucle for para sumar poblacion18
+			//procesar.....
+			map.addLayer(analysisLayer);
+			
+			map.zoomToMaxExtent();
+
+        },
+        failure: function (response) {
+            alert("Something went wrong in the request");
+        }
+    });
+
+
+}
 function getRoute(latinit,loninit,lat,lon){
-var request=`
-<?xml version="1.0" encoding="UTF-8"?>
+var requestRoute=`<?xml version="1.0" encoding="UTF-8"?>
 <xls:XLS xmlns:xls="http://www.opengis.net/xls"
 xmlns:gml="http://www.opengis.net/gml"
 xmlns:sch="http://www.ascc.net/xml/schematron"
@@ -181,7 +328,6 @@ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 xsi:schemaLocation="http://www.opengis.net/xls http://schemas.opengis.net/ols/1.1.0/RouteService.xsd"
 xmlns:xlsext="http://www.opengis.net/xlsext" version="1.2" xls:lang="fr">
 <xls:RequestHeader />
-
 <xls:Request maximumResponses="10" methodName="RouteRequest" requestID="uid1460559761783_519" version="1.2">
 <xls:DetermineRouteRequest distanceUnit="KM">
 	<xls:RoutePlan>
@@ -190,27 +336,27 @@ xmlns:xlsext="http://www.opengis.net/xlsext" version="1.2" xls:lang="fr">
             <xls:WayPointList>
             	<xls:StartPoint>
                 	<xls:Position>
-                		<gml:Point srsName="EPSG:3857"><gml:pos>${latinit} ${loninit}</gml:pos></gml:Point>
+                		<gml:Point srsName="EPSG:4326"><gml:pos>${latinit} ${loninit}</gml:pos></gml:Point>
                     </xls:Position>
                 </xls:StartPoint>
                 <xls:EndPoint>
                 	<xls:Position>
-                        <gml:Point srsName="EPSG:3857"><gml:pos>${lat} ${lon}</gml:pos></gml:Point>
+                        <gml:Point srsName="EPSG:4326"><gml:pos>${lat} ${lon}</gml:pos></gml:Point>
                     </xls:Position>
                 </xls:EndPoint>
             </xls:WayPointList>
     </xls:RoutePlan>
 	<xls:RouteInstructionsRequest provideGeometry="true"/>
 </xls:DetermineRouteRequest>
-<xls:RouteGeometryRequest/></xls:Request></xls:XLS>
-`;
+<xls:RouteGeometryRequest/></xls:Request></xls:XLS>`
 
-//var olsserver='http://wxs.ign.fr/1tqlxrqh5kdy2lgh6kmpiuhh/itineraire/ols';
+var olsserver='http://wxs.ign.fr/yvmoikafaddadzmxvh6sdmjb/itineraire/ols';
 var olsserver='http://localhost/ols';
 
 var request = new OpenLayers.Request.POST({
+       	
         url: olsserver,
-        data: request,
+        data: requestRoute,
         headers: {
             "Content-Type": "text/xml;charset=utf-8"
         },
@@ -218,16 +364,13 @@ var request = new OpenLayers.Request.POST({
         callback: function (response) {
 
             //read the response from GeoServer
-            var features = readFeaturesFromWPSResponse(response.responseText);
-
-			// features debe tener una coleccion de features con la ruta
-			getAnalysisFeatures(features);
-
+            processOLSResponse(response.responseText);
 
         },
         failure: function (response) {
             alert("Something went wrong in the request");
         }
+
     });
 
 }
