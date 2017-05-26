bcfb6fe4c4ee223a15dbe1002c1c154fb28152a6
Juan Pablo de Castro <jpdecastro@tel.uva.es>
2017-05-26 17:36:50 +0200

    Preserve active configurelocalvars.js to allow updating the server files.

--- 2017/EquipoB/configurelocalvars-dist.js
+++ 2017/EquipoB/configurelocalvars-dist.js
@@ -1,2 +1,6 @@
+/**
+ * This file is for configuration of urls of the app.
+ * Edit and rename to configuralocalvars.js
+ */
 var wfsServerUrl = 'https://localhost:8080/geoserver/wfs',
     wpsCartociudadUrl = 'https://www.cartociudad.es/wps/WebProcessingService';
